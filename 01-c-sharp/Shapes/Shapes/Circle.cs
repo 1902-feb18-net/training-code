881b0da2e61f8f372423a27dc683258331e0a60d
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-02-19 16:34:18 -0600

    add shapes circle example

---
+++ 01-c-sharp/Shapes/Shapes/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    class Circle : IShape
+    {
+        private double _radius = 0;
+
+        public double Length
+        {
+            get => _radius * 2;
+            set
+            {
+                _radius = value / 2;
+            }
+        }
+
+        public double Width
+        {
+            get => _radius * 2;
+            set
+            {
+                _radius = value / 2;
+            }
+        }
+    }
+}
---
+++ 01-c-sharp/Shapes/Shapes/Ellipse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    class Ellipse : IShape
+    {
+        public virtual double Length { get; set; }
+        public virtual double Width { get; set; }
+    }
+}
