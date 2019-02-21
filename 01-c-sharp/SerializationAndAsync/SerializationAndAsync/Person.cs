dc1912296d63479078b431b01e20731b11b77b76
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-02-21 16:06:13 -0600

    serialize persons to file.

---
+++ 01-c-sharp/SerializationAndAsync/SerializationAndAsync/Person.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Serialization;
+
+namespace SerializationAndAsync
+{
+    // classes to be serialized/deserialized (DTOs, data transfer objects)
+    // should be POCOs (plain old C# objects)
+    // which means, it must have a zero-parameter constructor
+    // and public get-set properties
+
+    public class Person
+    {
+        [XmlAttribute]
+        public int Id { get; set; }
+
+        [XmlAttribute("FullName")]
+        public string Name { get; set; }
+
+        [XmlElement(ElementName = "StreetAddress")]
+        public Address Address { get; set; }
+    }
+}
---
+++ 01-c-sharp/SerializationAndAsync/SerializationAndAsync/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace SerializationAndAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = new List<Person>
+            {
+                new Person
+                {
+                    Id = 1,
+                    Name = "Nick",
+                    Address = new Address
+                    {
+                        Street = "123 Main St",
+                        City = "Fort Worth",
+                        State = "TX"
+                    }
+                },
+                new Person
+                {
+                    Id = 2,
+                    Name = "Fred",
+                    Address = new Address
+                    {
+                        Street = "123 Main St",
+                        City = "Reston",
+                        State = "VA"
+                    }
+                }
+            };
+
+            // to send this over network or to disk, we need to serialize it.
+            // meaning, collecting data from across memory locations
+            // into a well-defined text or binary format.
+            // ideally this is reversible, we can deserialize the data back from
+            // its format into memory (maybe on the other end of the network connection.)
+
+            // normally, \ (backslash) is used as an escape character in string literals.
+            // so, this string has a new line character:
+            string newline = "\n";
+            // when we want to treat backslashes literally, we have @ strings...
+            string fileName = @"C:\revature\persons_data.xml";
+
+            SerializeAsXMLToFile(fileName, persons);
+        }
+
+        private static void SerializeAsXMLToFile(string fileName, List<Person> persons)
+        {
+            // our first object, XmlSerializer
+            // unfortunately does not know about generics
+            var serializer = new XmlSerializer(typeof(List<Person>));
+
+            // create mode, to overwrite file if already exists.
+            FileStream fileStream = null;
+
+            try
+            {
+                fileStream = new FileStream(fileName, FileMode.Create);
+
+                serializer.Serialize(fileStream, persons);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("error in writing to file:");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                fileStream?.Dispose(); // all IDisposable have Dispose method.
+            }
+        }
+    }
+}
