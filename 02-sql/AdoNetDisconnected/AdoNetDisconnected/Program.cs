2e161f4742acfb1f68c9b2627999859b729fbb2a
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-02-28 13:45:41 -0600

    disconnected empty project with proper gitignore

---
+++ 02-sql/AdoNetDisconnected/AdoNetDisconnected/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdoNetDisconnected
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
