e2d3432a1e56a896c39f6376335f4ea5968cb99c
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-02-28 15:06:55 -0600

    scaffold to fluent api

--- 02-sql/HelloEntityFramework/HelloEntityFramework/Program.cs
+++ 02-sql/HelloEntityFramework/HelloEntityFramework/Program.cs
@@ -11,7 +11,29 @@ namespace HelloEntityFramework
         // 3. add NuGet packages to the startup project:
         //     - Microsoft.EntityFrameworkCore.Tools
         //     - Microsoft.EntityFrameworkCore.SqlServer
+        //    and to the data access project:
+        //     - Microsoft.EntityFrameworkCore.SqlServer
+        // 4. open Package Manager Console in VS
+        //     ( View -> Other Windows -> Package Manager Console
+        // 5. run command (your solution needs to be able to compile) (one-line):
+        //      Scaffold-DbContext "<your-connection-string>"
+        //             Microsoft.EntityFrameworkCore.SqlServer
+        //             -Project <name-of-data-project> -Force
+        // (alternate 4/5. run in git bash/terminal:
+        //      dotnet ef dbcontext scaffold "<your-connection-string>"
+        //               Microsoft.EntityFrameworkCore.SqlServer
+        //               --project <name-of-data-project> -Force
+        // 6. delete the OnConfiguring override in the DbContext, to prevent
+        //       committing your connection string to git.
 
+        // (7. any time we change the database (add a new column, etc.), go to step 4.)
+
+
+        // by default, the scaffolding will configure the models in OnModelCreating
+        //     with the fluent API. this is the right way to do it - strongest separation
+        //     of concerns, more flexibility.
+        //   if we scaffold with option "-DataAnnotations" we'll put the configuration
+        //   on the Movie and Genre classes themselves with attributes.
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
