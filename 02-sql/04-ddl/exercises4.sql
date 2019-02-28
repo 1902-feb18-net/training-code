5081f9f18554a3a2c6adecec1dab3494e30f1b38
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-02-28 10:32:34 -0600

    add function exercises

---
+++ 02-sql/04-ddl/exercises4.sql
@@ -0,0 +1,13 @@
+-- exercises
+
+-- 1. write a function to get the initials of a customer based on his ID
+
+-- 2. create a function that returns the length of a track
+
+-- 3. create a function that returns the average total of all invoices
+
+-- 4. create a function that returns the most expensive track
+
+-- 5. create a function that returns the average price of all invoice items
+
+-- 6. create a function that, given an employee, returns all the employees he manages (that report to him).
