f39664e2b4de012717cbdafdb9a5396a4a5eca1e
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-02-28 14:31:48 -0600

    first couple steps to db-first ef.

--- 02-sql/HelloEntityFramework/HelloEntityFramework/Program.cs
+++ 02-sql/HelloEntityFramework/HelloEntityFramework/Program.cs
@@ -4,6 +4,14 @@ namespace HelloEntityFramework
 {
     class Program
     {
+        // EF database-first approach steps:
+        //
+        // 1. have startup project, and data access library project.
+        // 2. reference data access from startup project.
+        // 3. add NuGet packages to the startup project:
+        //     - Microsoft.EntityFrameworkCore.Tools
+        //     - Microsoft.EntityFrameworkCore.SqlServer
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
