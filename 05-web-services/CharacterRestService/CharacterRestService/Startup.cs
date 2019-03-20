bdf0174d906060b066baa55e4e03d8bedb638568
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-03-20 15:55:29 -0500

    add swagger

--- 05-web-services/CharacterRestService/CharacterRestService/Startup.cs
+++ 05-web-services/CharacterRestService/CharacterRestService/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace CharacterRestService
 {
@@ -24,6 +25,12 @@ namespace CharacterRestService
             })
                 .AddXmlSerializerFormatters()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Character API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +47,16 @@ namespace CharacterRestService
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Character API V1");
+            });
+
             app.UseMvc();
         }
     }
