6519cff20061eab1c7a78598970368d7309b0052
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-03-21 23:01:19 -0500

    add register in mvc.

---
+++ 05-web-services/CharacterMvc/CharacterMvc/ApiModels/ApiRegister.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CharacterMvc.ApiModels
+{
+    public class ApiRegister
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string Username { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [RegularExpression(@"(?=.*[a-z])(?=.*[0-9])",
+            ErrorMessage = "The password must have a lowercase letter and a digit.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
--- 05-web-services/CharacterMvc/CharacterMvc/Controllers/AccountController.cs
+++ 05-web-services/CharacterMvc/CharacterMvc/Controllers/AccountController.cs
@@ -17,14 +17,16 @@ namespace CharacterMvc.Controllers
         {
         }
 
-        public IActionResult Login()
+        // GET: /Account/Login
+        public ActionResult Login()
         {
             return View();
         }
 
+        // POST: /Account/Login
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login(ApiLogin login)
+        public async Task<ActionResult> Login(ApiLogin login)
         {
             if (!ModelState.IsValid)
             {
@@ -70,8 +72,9 @@ namespace CharacterMvc.Controllers
 
         // should also have register and logout
 
+        // POST: /Account/Logout
         [HttpPost]
-        public async Task<IActionResult> Logout()
+        public async Task<ActionResult> Logout()
         {
             HttpRequestMessage request = CreateRequestToService(HttpMethod.Post,
                 Configuration["ServiceEndpoints:AccountLogout"]);
@@ -101,9 +104,57 @@ namespace CharacterMvc.Controllers
             return RedirectToAction("Index", "Home");
         }
 
-        public IActionResult Register()
+        // GET: /Account/Register
+        public ActionResult Register()
         {
             return View();
+        }
+
+        // POST: /Account/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(ApiRegister register)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(register);
+            }
+
+            HttpRequestMessage request = CreateRequestToService(HttpMethod.Post,
+                Configuration["ServiceEndpoints:AccountRegister"], register);
+
+            HttpResponseMessage response;
+            try
+            {
+                response = await HttpClient.SendAsync(request);
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Unexpected server error");
+                return View(register);
+            }
+
+            if (!response.IsSuccessStatusCode)
+            {
+                if (response.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    // login failed because bad credentials
+                    ModelState.AddModelError("", "Login or password incorrect.");
+                    return View(register);
+                }
+                ModelState.AddModelError("", "Unexpected server error");
+                return View(register);
+            }
+
+            var success = PassCookiesToClient(response);
+            if (!success)
+            {
+                ModelState.AddModelError("", "Unexpected server error");
+                return View(register);
+            }
+
+            // login success
+            return RedirectToAction("Index", "Home");
         }
 
         private bool PassCookiesToClient(HttpResponseMessage apiResponse)
