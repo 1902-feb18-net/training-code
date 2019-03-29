a4f8f656c69a47a3b5940b1b83cea5acec3f7709
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-03-29 13:39:37 -0500

    fix namespaces from previous

--- 05-web-services/CharacterMvc/CharacterMvc/Startup.cs
+++ 05-web-services/CharacterMvc/CharacterMvc/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace CharacterMvc
 {
@@ -23,6 +24,8 @@ namespace CharacterMvc
             Configuration = configuration;
             _logger = logger;
         }
+
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
